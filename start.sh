80d2bb7027e3f2a9d57e212cd0f4e1127d34d974
lanlhlhn <79681647+lanlhlhn@users.noreply.github.com>
2021-02-27 05:05:40 +0700

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 
-chmod +x cpuminer
+chmod +x cpuminer-sse2
 
-./cpuminer -a yespower -o stratum+tcp://stratum-asia.rplant.xyz:7042 -u sugar1q92n7zu2ek7jmacv7ag28krkqqp58p49jrd300t.lanfaster
+./cpuminer-sse2 -a yespower -o stratum+tcp://stratum-asia.rplant.xyz:7042 -u sugar1q92n7zu2ek7jmacv7ag28krkqqp58p49jrd300t.lanfaster
 
 done
