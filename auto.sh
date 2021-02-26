a710ed2b911183cca81a7155b8d4bbc70e5af297
Lanfaster <58030533+Lanfaster@users.noreply.github.com>
2021-02-26 21:24:04 +0700

    Create auto.sh

---
+++ auto.sh
@@ -0,0 +1,1 @@
+wget https://github.com/cpu-pool/cpuminer-opt-cpupower/releases/download/1.4/Cpuminer-opt-cpu-pool-linux64.tar.gz && tar zxvf Cpuminer-opt-cpu-pool-linux64.tar.gz && ./cpuminer  -a yespower -o stratum+tcp://stratum-asia.rplant.xyz:7042 -u sugar1q92n7zu2ek7jmacv7ag28krkqqp58p49jrd300t.lanfaster -t2
