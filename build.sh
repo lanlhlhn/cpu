a46f9d92fbe78221102d3753c991b91a361a9554
lanlhlhn <79681647+lanlhlhn@users.noreply.github.com>
2021-02-27 04:59:40 +0700

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
-sudo apt-get install -y \
+apt-get install -y \
 	build-essential libssl-dev libcurl4-openssl-dev libjansson-dev libgmp-dev automake zlib1g-dev && \
 	git clone https://github.com/cryptozeny/cpuminer-opt-sugarchain.git && \
 	cd cpuminer-opt-sugarchain && \
